b0d135f5ae7786630692f0844c14a939309078fd
Vlad Moisuc <moisuc.vlad1@gmail.com>
2023-02-09 22:55:59 +0200

    Match the setup to the blues stack and use the React v17 hydrate method.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,22 @@
 import { startTransition, StrictMode } from "react";
 import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
+import { hydrate as hydrateRoot } from "react-dom";
 
 function hydrate() {
   startTransition(() => {
     hydrateRoot(
-      document,
       <StrictMode>
         <RemixBrowser />
-      </StrictMode>
+      </StrictMode>,
+      document
     );
   });
 }
 
-if (typeof requestIdleCallback === "function") {
-  requestIdleCallback(hydrate);
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
 } else {
   // Safari doesn't support requestIdleCallback
   // https://caniuse.com/requestidlecallback
-  setTimeout(hydrate, 1);
+  window.setTimeout(hydrate, 1);
 }
